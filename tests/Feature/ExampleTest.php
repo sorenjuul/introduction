c9d52708f1fd239d960698c20a9bb735514ff2cd
Soren Juul <sorenjuul@gmail.com>
2018-02-16 14:06:05 +0100

    Added a couple of feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testCustomersPage()
     {
         $response = $this->get('/');
 
@@ -22,4 +22,24 @@ class ExampleTest extends TestCase
         $response->assertSeeText('Jørgen');
         $response->assertSeeText('Peter');
     }
+
+    public function testInvoicesPage() {
+        $this->get('/customer/invoice/1');
+        $response = $this->get('/invoices');
+
+        $response->assertStatus(200);
+        $response->assertSeeText('Jørgen');
+        $response->assertDontSeeText('Peter');
+    }
+
+    public function testCustomerPage() {
+        $response = $this->get('/customer/1');
+
+        $response->assertStatus(200);
+        $response->assertSeeText('Jørgen');
+        $response->assertSeeText('DKK 12 weekly');
+        $response->assertSee('Invoice customer');
+        $response->assertDontSeeText('Peter');
+    }
+    
 }
